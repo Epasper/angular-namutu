d49420cfb2cb8dc7075a2e7a18bf7cfe6f48c419
Epasper <epasper@gmail.com>
2019-07-25 11:50:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-namutu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-namutu)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+
+
+export class ProductDetailsComponent implements OnInit {
+  product;
+
+  ngOnInit() {
+  this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productNumber')];
+  });
+}
+
+  constructor(
+    private route: ActivatedRoute,
+    
+  ) { }
+  
+
+
+}
